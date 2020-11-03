31378992028e1d19f525288b872efed1abc872f9
Mater <baixh@rd.netease.com>
2020-11-03 17:11:39 +0800

    feat: 添加demo

--- example/vue.config.js
+++ example/vue.config.js
@@ -2,13 +2,12 @@
  * @author : Mater
  * @Email : bxh8640@gmail.com
  * @Date : 2020-11-02 14:16:02
- * @LastEditTime : 2020-11-03 15:29:24
+ * @LastEditTime : 2020-11-03 17:10:38
  * @Description :
  */
 module.exports = {
+  publicPath: './',
   chainWebpack: function(config) {
-    config.module
-      .rule("js")
-      .exclude.add(/luban-h5-editor.js/);
+    config.module.rule('js').exclude.add(/luban-h5-editor.js/)
   }
-};
+}
