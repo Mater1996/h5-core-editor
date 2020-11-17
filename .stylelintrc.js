b2e0735acabca3775004f2797c3df27df71f27fe
Mater <baixh@rd.netease.com>
2020-11-17 11:56:57 +0800

    refactor: stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,9 +2,10 @@
  * @author : Mater
  * @Email : bxh8640@gmail.com
  * @Date : 2020-11-17 11:42:36
- * @LastEditTime : 2020-11-17 11:42:46
+ * @LastEditTime : 2020-11-17 11:52:23
  * @Description :
  */
 module.exports = {
-  extends: 'stylelint-config-standard'
+  plugins: ['stylelint-scss'],
+  extends: ['stylelint-config-standard-scss']
 }
