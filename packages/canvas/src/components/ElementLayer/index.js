9cad2f18ffcf4341e7e4782b0263bb5474d3a474
mater <bxh8640@gmail.com>
2021-03-05 11:37:25 +0800

    fix: luban plugin z-index 造成的移动事件错误

--- packages/canvas/src/components/ElementLayer/index.js
+++ packages/canvas/src/components/ElementLayer/index.js
@@ -2,7 +2,7 @@
  * @author : Mater
  * @Email : bxh8640@gmail.com
  * @Date : 2020-11-13 10:09:46
- * @LastEditTime: 2021-03-05 10:22:51
+ * @LastEditTime: 2021-03-05 11:32:21
  * @Description :
  */
 import Element from './element'
@@ -17,7 +17,7 @@ export default {
   },
   render () {
     return (
-      <div class="element-layer absolute left-0 top-0 right-0 bottom-0">
+      <div class="element-layer absolute left-0 top-0 right-0 bottom-0 z-0">
         {this.elements.map(element => {
           return <Element element={element}/>
         })}
